0256ad681079df5b48b32a357990b3941a559fc4
Divine_Sister <divinesister97@gmail.com>
2022-06-29 20:47:56 -0500

    Adding Cart, FONTS, Shadows

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,12 +4,23 @@ const COLORS = {
 }
 
 const SIZES = {
+    paddind: 19,
+    borderRadius:15,
+    textBorderRadius: 25,
     h1:24,
-    h2:20
+    h2:20,
 }
 
 const FONTS = {
-    h1_pacifico: { fontSize: SIZES.h1, fontFamily: ""}
+    h1_pacifico: { fontSize: SIZES.h1},
+    h2_montserrat: { fontSize: SIZES.h2}
 }
 
-export {COLORS, SIZES, FONTS}
+const SHADOW ={
+    elevation: 5,
+    shadowColor: COLORS.secondary,
+    shadowOffset: {width: 2, height: 12},
+    shadowRadius: 12,
+}
+
+export {COLORS, SIZES, FONTS, SHADOW}
